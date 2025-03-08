83c070488c113b696ce0be0f21c0f292333df5d9
Limonchik-D <DavLimon4@yandex.com>
2025-03-08 18:06:49 +0200

    Add files via upload

---
+++ Task 2 pr 27/Car.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2_PR_27
+{
+    class Car //Описание автомобиля
+    {
+        string make; //Марка автомобиля
+        string model; //Модель автомобиля
+        int year; //Год выпуска автомобиля
+
+        //Свойства для доступа к полям
+        public string Make
+        {
+            get { return make; }
+            set { make = value; }
+        }
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+
+        //Конструкторы
+        public Car()
+        {
+            make = "Toyota";
+            model = "Corolla";
+            year = 2010;
+        }
+
+        public Car(string make, string model, int year)
+        {
+            this.make = make;
+            this.model = model;
+            this.year = year;
+        }
+
+        //Метод для вывода информации об автомобиле
+        public override string ToString()
+        {
+            return $"Марка: {Make}, Модеь: {Model}, Год выпуска: {Year}";
+        }
+
+
+    }
+}
---
+++ Task 2 pr 27/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task_2_PR_27;
+
+namespace Task_2_pr_27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Инициализация массива объектов Car
+            Car[] car = new Car[]
+            {
+                new Car("BMW", "X5", 2020),
+                new Car("BMW", "X4", 2010),
+                new Car("BMW", "X3", 2000)
+            };
+
+            Console.WriteLine("Инициализированный массив:");
+            foreach (var cars in car)
+            {
+                Console.WriteLine(cars.ToString());
+            }
+
+            // Заполнение массива с клавиатуры
+            Car[] car1 = new Car[3];
+            for (int i = 0; i < car1.Length; i++)
+            {
+                Console.WriteLine("Введите марку автомобиля:");
+                string make = Console.ReadLine();
+
+                Console.WriteLine("Введите модель автомобиля:");
+                string model = Console.ReadLine();
+
+                Console.WriteLine("Введите год производства:");
+                int year;
+                while (!int.TryParse(Console.ReadLine(), out year)) // Проверка ввода года
+                {
+                    Console.WriteLine("Ошибка! Введите корректный год:");
+                }
+
+                car1[i] = new Car(make, model, year); // Сохранение объекта в массив
+            }
+
+ 
+            Console.WriteLine("\nМассив, заполненный с клавиатуры:");
+            foreach (var cars in car1)
+            {
+                Console.WriteLine(cars.ToString());
+            }
+
+            Console.ReadKey(); // Ожидание нажатия клавиши перед завершением
+        }
+    
+    }
+}
