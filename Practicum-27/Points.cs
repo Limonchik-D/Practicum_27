4aebdc987968250b571d550a77990bf52bf9fd3f
Limonchik-D <DavLimon4@yandex.com>
2025-03-08 18:06:16 +0200

    Add files via upload

---
+++ Practicum-27/Points.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicum_27
+{
+    class Points
+    {
+        private double x;
+        private double y;
+
+        /// <summary>
+        ///  свойства для доступа к полям
+        /// </summary>
+        public double X
+        {
+            get { return x;  }
+            set { x = value;  }
+        }
+        
+        public double Y
+        {
+            get { return y;  }
+            set { y = value;  }
+        }
+
+        public Points ()
+        {
+            x = 4.5;
+            y = 3.5;
+        }
+        public Points(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"Х: {x} Y: {y}";
+        }
+    }
+}
---
+++ Practicum-27/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicum_27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Points[] points = new Points[]
+            {
+                    new Points (4, 2),
+                    new Points (3.5, 4.9),
+                    new Points (4, 6)
+            };
+            foreach (var point in points)
+            {
+                Console.WriteLine(point.ToString());
+            }
+            Console.Read();
+        }
+    }
+}
